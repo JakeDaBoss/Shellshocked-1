bc8a82d55602f9ecf6f4509ab985f9e6b3216840
Jake Miles <visiongamingjake@hotmail.com>
2017-08-10 13:48:22 +0200

    Update to forums command plugin.

--- cwhl2rp/plugins/forums/plugin/commands/sh_forums.lua
+++ cwhl2rp/plugins/forums/plugin/commands/sh_forums.lua
@@ -4,7 +4,7 @@ COMMAND.flags = CMD_DEFAULT;
 
 -- Thanks to NightAngel for showing me this function instead of me making my own!
 function COMMAND:OnRun(player)
-     player:SendLua([[gui.OpenURL("Forums name!")]])
+     player:SendLua([[gui.OpenURL("http://steamcommunity.com/groups/shellshockedww3")]]) -- Changed this to the group. This can be changed later on to an actual forum.
 	 Clockwork.player:Notify(player, "Bringing you to our website now!")
 end;
 
